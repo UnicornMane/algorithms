e197d3b9d80568bf5b678c3d05506fbb396a2b0b
Alexandr Isaenkov <48714228+UnicornMane@users.noreply.github.com>
2019-11-19 20:19:59 +0300

    Add files via upload

---
+++ bfs.cpp
@@ -0,0 +1,49 @@
+﻿#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+	int n, v;
+	cin >> n >> v;
+	vector <vector <int>> g(n);
+	v--;
+	g[v].push_back(v + 1);
+	for (auto i = 0; i < n - 2; i++)
+	{
+		int start, finish;
+		cin >> start >> finish;
+		--start; --finish;
+		g[start].push_back(finish);
+		g[finish].push_back(start);
+	}
+	
+	//constexpr int inf = numeric_limits<int>::max();
+
+	const int inf = 1e9;
+
+	vector <int> dist(n, inf), prev(n);
+	dist[v] = 0;
+	queue <int> q;
+	q.push(v);
+	prev[0] = -1;
+	while (!q.empty())
+	{
+		const auto u = q.front();
+		q.pop();
+		for (auto i : g[u])
+		{
+			//u -> i ребро
+			if (dist[i] == inf)
+			{
+				prev[i] = u;
+				dist[i] = dist[u] + 1;
+				q.push(i);
+			}
+			//else if (dist[i] == prev[i])
+		}
+	}
+}
